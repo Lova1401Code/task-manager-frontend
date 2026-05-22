970c6c0ade3f56f234dea116bc55954afc0430b0
Lova1401Code <lova.ramiharisoa@gmail.com>
2026-05-22 22:56:22 +0300

    feat: add heroicons and refactor Navigator component with new icons

--- src/pages/Navigator.jsx
+++ src/pages/Navigator.jsx
@@ -1,36 +1,39 @@
-import icon from '../assets/icons/icon.png';
-import message from '../assets/icons/message.png';
+import IconDashboard from '../assets/IconDashboard';
+import IconMessage from '../assets/IconMessage';
+import IconTask from '../assets/IconTask';
 import navigator from '../assets/icons/navigator.png';
 import setting from '../assets/icons/settings.png';
 import team from '../assets/icons/team.png';
 import task from '../assets/icons/task.png'; 
-import dashboard from '../assets/icons/dashboard.png'
+import IconSettings from '../assets/IconSettings';
+import IconTeams from '../assets/IconTeams';
+import IconFiles from '../assets/IconFiles';
 
 export default function Navigator() {
     return <div className="bg-white flex flex-col my-4">
-            <div className="h-16 flex items-center justify-center">
-                <span className="text-white text-lg bg-blue-600 p-1">LG</span>
+            <div className="h-12 flex items-center justify-center">
+                <span className="text-white text-lg bg-blue-400 px-1 rounded">LG</span>
             </div>
 
-            <div className="flex-1 flex flex-col justify-between items-center py-4">
-                <div className="flex flex-col space-y-4 items-center">
-                    <div className="w-8 h-8 bg-green flex justify-center items-center  rounded">
-                        <img src={icon} alt="Icon" className="w-5 h-5" />
+            <div className="flex-1 flex flex-col justify-between items-center py-14">
+                <div className="w-full flex flex-col space-y-1 items-center">
+                    <div className="w-full h-12 bg-green flex justify-center items-center  rounded">
+                        <IconDashboard />
                     </div>
-                    <div className="w-8 h-8 bg-green flex justify-center items-center  rounded">
-                        <img src={message} alt="Message" className="w-5 h-5" />
+                    <div className="w-full h-12 bg-green flex justify-center items-center  rounded">
+                        <IconMessage />
                     </div>
-                    <div className="w-8 h-8 bg-green flex justify-center items-center  rounded">
-                        <img src={dashboard} alt="Dashboard" className="w-5 h-5" />
+                    <div className="border-blue-400 border-l-2 w-full h-12 bg-green flex justify-center items-center  rounded">
+                        <IconTask />
                     </div>
-                    <div className="w-8 h-8 flex justify-center items-center bg-white/20 rounded">
-                        <img src={setting} alt="Setting" className="w-5 h-5" />
+                    <div className="w-full h-12 flex justify-center items-center bg-white/20 rounded">
+                        <IconSettings />
                     </div>
-                    <div className="w-8 h-8 flex justify-center items-center bg-white/20 rounded">
-                        <img src={team} alt="Team" className="w-5 h-5" />
+                    <div className="w-full h-12 flex justify-center items-center bg-white/20 rounded">
+                        <IconTeams />
                     </div>
-                    <div className="w-8 h-8 flex justify-center items-center bg-white/20 rounded">
-                        <img src={task} alt="Task" className="w-5 h-5" />
+                    <div className="w-full h-12 flex justify-center items-center bg-white/20 rounded">
+                        <IconFiles />
                     </div>
                 </div>
             </div>*
