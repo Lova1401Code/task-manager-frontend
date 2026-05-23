f98bd61d876e8c3ea0274ba6fe56afdbadacc3bf
Lova1401Code <lova.ramiharisoa@gmail.com>
2026-05-23 10:27:13 +0300

    feat: refactor Navigator component and add new icon components with active state styling

--- src/assets/icons/IconTask.jsx
+++ src/assets/icons/IconTask.jsx
@@ -1,4 +1,4 @@
-export default function IconMessage() {
+export default function IconTask({ estActif }) {
     return (
     <svg 
         xmlns="http://www.w3.org/2000/svg" 
@@ -6,7 +6,7 @@ export default function IconMessage() {
         viewBox="0 0 24 24" 
         strokeWidth={1.5} 
         stroke="currentColor" 
-        className="size-6"
+        className={`size-6 ${estActif ? 'text-blue-400' : 'text-black'}`}
     >
         <path strokeLinecap="round" strokeLinejoin="round" d="M9 4.5v15m6-15v15m-10.875 0h15.75c.621 0 1.125-.504 1.125-1.125V5.625c0-.621-.504-1.125-1.125-1.125H4.125C3.504 4.5 3 5.004 3 5.625v12.75c0 .621.504 1.125 1.125 1.125Z" />
     </svg>
--- src/pages/Navigator.jsx
+++ src/pages/Navigator.jsx
@@ -1,15 +1,17 @@
-import IconDashboard from '../assets/IconDashboard';
-import IconMessage from '../assets/IconMessage';
-import IconTask from '../assets/IconTask';
-import navigator from '../assets/icons/navigator.png';
-import setting from '../assets/icons/settings.png';
-import team from '../assets/icons/team.png';
-import task from '../assets/icons/task.png'; 
-import IconSettings from '../assets/IconSettings';
-import IconTeams from '../assets/IconTeams';
-import IconFiles from '../assets/IconFiles';
+import { useState } from 'react';
+import IconDashboard from '../assets/icons/IconDashboard';
+import IconMessage from '../assets/icons/IconMessage';
+import IconTask from '../assets/icons/IconTask';
+import IconSettings from '../assets/icons/IconSettings';
+import IconTeams from '../assets/icons/IconTeams';
+import IconFiles from '../assets/icons/IconFiles';
 
 export default function Navigator() {
+    const [activeIcon, setActiveIcon] = useState('Dashboard');
+    const iconClick = (nameIcon) => {
+        setActiveIcon(nameIcon);
+
+    }
     return <div className="bg-white flex flex-col my-4">
             <div className="h-12 flex items-center justify-center">
                 <span className="text-white text-lg bg-blue-400 px-1 rounded">LG</span>
@@ -17,24 +19,98 @@ export default function Navigator() {
 
             <div className="flex-1 flex flex-col justify-between items-center py-14">
                 <div className="w-full flex flex-col space-y-1 items-center">
-                    <div className="w-full h-12 bg-green flex justify-center items-center  rounded">
-                        <IconDashboard />
-                    </div>
-                    <div className="w-full h-12 bg-green flex justify-center items-center  rounded">
-                        <IconMessage />
-                    </div>
-                    <div className="border-blue-400 border-l-2 w-full h-12 bg-green flex justify-center items-center  rounded">
-                        <IconTask />
-                    </div>
-                    <div className="w-full h-12 flex justify-center items-center bg-white/20 rounded">
-                        <IconSettings />
-                    </div>
-                    <div className="w-full h-12 flex justify-center items-center bg-white/20 rounded">
-                        <IconTeams />
-                    </div>
-                    <div className="w-full h-12 flex justify-center items-center bg-white/20 rounded">
-                        <IconFiles />
-                    </div>
+                    <button 
+                        className={
+                            `${
+                            activeIcon === 'Dashboard' ? 'border-blue-400 border-l-2' : ''
+                            } 
+                            w-full 
+                            h-12 
+                            bg-green 
+                            flex 
+                            justify-center 
+                            items-center  
+                            rounded`    
+                        }
+                        onClick={() => iconClick('Dashboard')}
+                    >
+                        <IconDashboard estActif={activeIcon === 'Dashboard'} />
+                    </button>
+                    <button 
+                        className={ 
+                                `${activeIcon === 'Message' ? 'border-blue-400 border-l-2' : ''} 
+                                w-full 
+                                h-12 
+                                bg-green 
+                                flex 
+                                justify-center 
+                                items-center  
+                                rounded`
+                           }
+                        onClick={() => iconClick('Message')}
+                    >
+                        <IconMessage estActif={activeIcon === 'Message'}/>
+                    </button>
+                    <button 
+                        className={ 
+                                `${activeIcon === 'Task' ? 'border-blue-400 border-l-2' : ''} 
+                                w-full 
+                                h-12 
+                                bg-green 
+                                flex 
+                                justify-center 
+                                items-center  
+                                rounded`
+                           }
+                        onClick={() => iconClick('Task')}
+                    >
+                        <IconTask estActif={activeIcon === 'Task'} />
+                    </button>
+                    <button 
+                        className={ 
+                                `${activeIcon === 'Settings' ? 'border-blue-400 border-l-2' : ''} 
+                                w-full 
+                                h-12 
+                                flex 
+                                justify-center 
+                                items-center 
+                                bg-white/20 
+                                rounded`
+                           }
+                        onClick={() => iconClick('Settings')}
+                    >
+                        <IconSettings estActif={activeIcon === 'Settings'} />
+                    </button>
+                    <button 
+                        className={ 
+                                `${activeIcon === 'Teams' ? 'border-blue-400 border-l-2' : ''} 
+                                w-full 
+                                h-12 
+                                flex 
+                                justify-center 
+                                items-center 
+                                bg-white/20 
+                                rounded`
+                           }
+                        onClick={() => iconClick('Teams')}
+                    >
+                        <IconTeams estActif={activeIcon === 'Teams'} />
+                    </button>
+                    <button 
+                        className={ 
+                                `${activeIcon === 'Files' ? 'border-blue-400 border-l-2' : ''} 
+                                w-full 
+                                h-12 
+                                flex 
+                                justify-center 
+                                items-center 
+                                bg-white/20 
+                                rounded`
+                           }
+                        onClick={() => iconClick('Files')}
+                    >
+                        <IconFiles estActif={activeIcon === 'Files'} />
+                    </button>
                 </div>
             </div>*
     </div>
