791a28013673ea5d4d6a33cb86182304d632b9c5
Lova1401Code <lova.ramiharisoa@gmail.com>
2026-05-28 21:57:27 +0300

    feat: add Chevron icons and update ExtensionBar to use them for menu toggle

--- src/pages/ExtensionBar.jsx
+++ src/pages/ExtensionBar.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import FolderIcon from "../assets/icons/FolderIcon";
+import ChevronDownMicorIcon from "../assets/icons/ChevronDownMicorIcon";
+import ChevronUpMicroIcon from "../assets/icons/ChevronUpMicroIcon";
 
 export default function ExtensionBar() {
     const [isOpen1, setIsOpen1] = useState(true);
@@ -15,29 +17,29 @@ export default function ExtensionBar() {
 
     return (
         <div className="flex flex-col w-full my-4 bg-white items-center p-2 gap-8">
-            <button className="h-8 bg-gray-100 hover:bg-gray-200 rounded w-full px-2 my-2">
+            <button className="h-8  bg-[#FAFAFA] hover:bg-gray-200 rounded w-full px-2 my-2">
                 <span className="text-gray-700 font-bold mr-2">+</span>
                 New Project
             </button>
             <div className="flex flex-col w-full gap-4 items-center">
                 <div className="w-full">
                     <button className="flex items-center p-1" onClick={menuClick1}>
-                        <span className="text-gray-700 mr-2">{isOpen1 ? '▼' : '▲'}</span>
+                        <span className="text-gray-700 mr-2">{isOpen1 ? <ChevronDownMicorIcon/> : <ChevronUpMicroIcon/>}</span>
                         <span>Marketing</span>
                     </button>
 
                     {isOpen1 && (
                         <ul className="flex flex-col gap-0 ml-2 text-gray-600">
-                            <li className="flex items-center p-1">
-                                <span className="text-gray-700 mr-2">•</span>
+                            <li className="flex items-center p-1 bg-[#EEF7FF] rounded">
+                                <span className="text-gray-700 mr-2"><FolderIcon /></span>
                                 <span>Sub-item 1</span>
                             </li>
                             <li className="flex items-center p-1">
-                                <span className="text-gray-700 mr-2">•</span>
+                                <span className="text-gray-700 mr-2"><FolderIcon /></span>
                                 <span>Sub-item 1</span>
                             </li>
                             <li className="flex items-center p-1">
-                                <span className="text-gray-700 mr-2">•</span>
+                                <span className="text-gray-700 mr-2"><FolderIcon /></span>
                                 <span>Sub-item 1</span>
                             </li>
                         </ul>
@@ -46,7 +48,7 @@ export default function ExtensionBar() {
                 </div>
                 <div className="w-full">
                     <button className="flex items-center" onClick={menuClick2}>
-                        <span className="text-gray-700 mr-3">{isOpen2 ? '▼' : '▲'}</span>
+                        <span className="text-gray-700 mr-3">{isOpen2 ? <ChevronDownMicorIcon/> : <ChevronUpMicroIcon/>}</span>
                         <span>Marketing</span>
                     </button>
 
@@ -57,11 +59,11 @@ export default function ExtensionBar() {
                                 <span>Sub-item 1</span>
                             </li>
                             <li className="flex items-center p-1">
-                                <span className="text-gray-700 mr-2">•</span>
+                                <span className="text-gray-700 mr-2"><FolderIcon /></span>
                                 <span>Sub-item 1</span>
                             </li>
                             <li className="flex items-center p-1">
-                                <span className="text-gray-700 mr-2">•</span>
+                                <span className="text-gray-700 mr-2"><FolderIcon /></span>
                                 <span>Sub-item 1</span>
                             </li>
                         </ul>
