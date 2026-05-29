8a9d2a731756fc2207defab3700814dfe73f07f8
Lova1401Code <lova.ramiharisoa@gmail.com>
2026-05-29 22:59:21 +0300

    feat: add PhoneIcon, VideoCameraIcon, and XIcon components; update ExtendProfile layout with new icons

--- src/pages/ExtendProfile.jsx
+++ src/pages/ExtendProfile.jsx
@@ -1,3 +1,20 @@
+import PhoneIcon from "../assets/icons/PhoneIcon"
+import VideoCameraIcon from "../assets/icons/VideoCameraIcon"
+import XIcon from "../assets/icons/XIcon"
 export default function ExtendProfile() {
-    return <div className="my-4 bg-white p-4">ExtendProfile</div>
+    return (
+    <div className="relative my-4 bg-white p-2">
+        <button className="p-1 absolute top-1 left-1">
+            <XIcon />
+        </button>
+        <div className="flex absolute top-1 right-1">
+            <button className="p-1">
+                <VideoCameraIcon />
+            </button>
+            <button className="p-1">
+                <PhoneIcon />
+            </button>
+        </div>
+    </div>
+    )
 }
