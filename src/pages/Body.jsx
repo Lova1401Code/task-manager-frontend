e33b745fd0592dfc584d7d40d887e0b5a3464e8a
Lova1401Code <lova.ramiharisoa@gmail.com>
2026-06-04 22:13:41 +0300

    feat: update Body component layout and content for improved presentation

--- src/pages/Body.jsx
+++ src/pages/Body.jsx
@@ -1,3 +1,13 @@
 export default function Body() {
-    return <div className="my-4 bg-white p-4">Body</div>
+    return (
+        <div className="my-4 bg-white p-3">
+            <div className="flex justify-between w-full">
+                <div className="text-lg font-semibold">Marketing Plane W23</div>
+                <div className="flex gap-2">
+                    <div className=""></div>
+                </div>
+                <div className="">Icon</div>
+            </div>
+         </div>
+    )
 }
