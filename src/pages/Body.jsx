f08fad1fee155089e47dad41c5486928256949a0
Lova1401Code <lova.ramiharisoa@gmail.com>
2026-06-05 06:39:50 +0300

    feat: enhance Body component with profile images and action icons

--- src/pages/Body.jsx
+++ src/pages/Body.jsx
@@ -1,12 +1,19 @@
+import profileUrl from '../assets/profile.png'
+import PlusIcon from '../assets/icons/PlusIcon'
+import SearchIcon from '../assets/icons/SearchIcon'
+
 export default function Body() {
     return (
         <div className="my-4 bg-white p-3">
-            <div className="flex justify-between w-full">
+            <div className="flex justify-between w-full items-center">
                 <div className="text-lg font-semibold">Marketing Plane W23</div>
-                <div className="flex gap-2">
-                    <div className=""></div>
+                <div className="flex justify-end items-center -space-x-4 gap-2">
+                    <img src={profileUrl} alt="profile" className="w-8 h-auto rounded-full" />
+                    <img src={profileUrl} alt="profile" className="w-8 h-auto rounded-full" />
+                    <img src={profileUrl} alt="profile" className="w-8 h-auto rounded-full" />
+                    <div className="flex pl-4"><PlusIcon /></div>
+                    <div className="flex pl-8"><SearchIcon /></div>
                 </div>
-                <div className="">Icon</div>
             </div>
          </div>
     )
