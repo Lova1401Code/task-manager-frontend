be9932038fd1a0c61f64fe8b128504ed2a324c90
Lova1401Code <lova.ramiharisoa@gmail.com>
2026-06-06 07:42:17 +0300

    feat: update Body component navigation and add button controls for view options

--- src/pages/Body.jsx
+++ src/pages/Body.jsx
@@ -15,8 +15,8 @@ export default function Body() {
                     <div className="flex pl-8"><SearchIcon /></div>
                 </div>
             </div>
-            <nav className="w-full ml-2 px-3">
-                <ul className="flex gap-4 justify-start items-center w-full">
+            <nav className="w-full pl-3 px-3 border-b-3 border-gray-100">
+                <ul className="flex gap-4 justify-start items-center w-full -mb-px">
                     <li className="text-sm text-[#C4C4C4]">Dashboard</li>
                     <li className="text-sm text-[#000000] font-semibold border-b-3 border-[#3BA1FF] py-1 px-2">Tasks</li>
                     <li className="text-sm text-[#C4C4C4]">Timelines</li> 
@@ -24,7 +24,16 @@ export default function Body() {
                     <li className="text-sm text-[#C4C4C4]">Overview</li>
                 </ul>
             </nav>
-            <div className="bg-[#FAFAFA]">a</div>
+            <div className="bg-[#FAFAFA]">
+                <div className="flex justify-between items-center p-4">
+                    <div className="flex justify-start items-center">
+                        <button className="text-xs bg-[#F0F0F0] px-2 py-1 rounded-full">Karban</button>
+                        <button className="text-xs px-2 py-1 rounded-full">Table</button>
+                        <button className="text-xs px-2 py-1 rounded-full">List view</button>
+                    </div> 
+                    <div className="flex justify-end items-center"><button className="text-xs px-2 py-1 rounded-full">List view</button></div>
+                </div>
+            </div>
          </div>
     )
 }
