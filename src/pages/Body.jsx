45a02b41058b665da514985c6d0a37a91009730a
Lova1401Code <lova.ramiharisoa@gmail.com>
2026-06-07 09:26:51 +0300

    feat: integrate tasks and status mapping into Body component for dynamic task display

--- src/pages/Body.jsx
+++ src/pages/Body.jsx
@@ -11,6 +11,16 @@ import PaperClipIcon from '../assets/icons/PaperClipIcon'
 import MessageIcon from '../assets/icons/MessageIcon'
 
 export default function Body() {
+    const tasks = [
+        { id: 1, title: "Task 1" },
+        { id: 2, title: "Task 2" }
+    ]
+    const status = [
+        { id: 1, name: "Pending" },
+        { id: 2, name: "In Progress" },
+        { id: 3, name: "Completed" },
+        { id: 4, name: "On Hold" }
+    ]
     return (
         <div className="flex flex-col bg-white">
             <div className="flex  p-3 justify-between w-full items-center">
@@ -57,54 +67,60 @@ export default function Body() {
                 </div>
 
                 <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-                    <div className="flex flex-col gap-0.5">
+                    { status.map((stat) => (
+                        <div className="flex flex-col gap-0.5">
                         <div className="flex justify-start items-center gap-1">
                             <span className="mr-1"><MarkIcon /></span>
-                            <h2 className="text-md font-semibold">Pending</h2>
+                            <h2 className="text-md font-semibold">{stat.name}</h2>
                         </div>
                         <button className="flex mt-2 gap-1 items-center justify-center w-full bg-white px-2 py-1 rounded">
                             <span className="mr-1"><PlusIcon /></span>
                             <span className="text-sm">Add New</span>
                         </button>
-                        <div className="flex flex-col gap-2 p-2 bg-white">
-                            <div className="flex gap-2">
-                                <button className="rounded-full bg-[#3BA1FF] px-2 py-1 text-white text-xs">Marketing Team</button>
-                                <button className="rounded-full bg-[#08BD51] px-2 py-1 text-white text-xs">UX/UI Design</button>
-                            </div>
-                            <p className="text-sm">No tasks to display</p>
-                        </div>
-                        <ul className="flex flex-col gap-2 p-2 bg-white">
-                            <li className="flex items-center gap-2">
-                                <span className="mr-1"><CheckIcon /></span>
-                                <span className="text-sm">Task 1</span>
-                            </li>
-                            <li className="flex items-center gap-2">
-                                <span className="mr-1"><CheckIcon /></span>
-                                <span className="text-sm">Task 1</span>
-                            </li>
-                            <li className="flex items-center gap-2">
-                                <span className="mr-1"><CheckIcon /></span>
-                                <span className="text-sm">Task 1</span>
-                            </li>
-                        </ul>
-                        <div className="flex justify-between p-2 bg-white">
-                            <div className="flex justify-start -space-x-2">
-                                <img src={profileUrl} alt="profile" className="w-6 h-auto rounded-full" />
-                                <img src={profileUrl} alt="profile" className="w-6 h-auto rounded-full" />
-                                <img src={profileUrl} alt="profile" className="w-6 h-auto rounded-full" />   
-                            </div>
-                            <div className="flex justify-end gap-2">
-                                <div className="flex items-center gap-1"> 
-                                    <span className=""><MessageIcon /></span>
-                                    <span className="text-sm text-[#C4C4C4]">1</span>
+                        {tasks.map((task) => (
+                            <div className="flex flex-col">
+                            <div className="flex flex-col gap-2 p-2 bg-white">
+                                <div className="flex gap-2">
+                                    <button className="rounded-full bg-[#3BA1FF] px-2 py-1 text-white text-xs">Marketing Team</button>
+                                    <button className="rounded-full bg-[#08BD51] px-2 py-1 text-white text-xs">UX/UI Design</button>
                                 </div>
-                                <div className="flex items-center gap-1"> 
-                                    <span className=""><PaperClipIcon /></span>
-                                    <span className="text-sm text-[#C4C4C4]">2</span>
+                                <p className="text-sm">No tasks to display</p>
+                            </div>
+                            <ul className="flex flex-col gap-2 p-2 bg-white">
+                                <li className="flex items-center gap-2">
+                                    <span className="mr-1"><CheckIcon /></span>
+                                    <span className="text-sm">Task 1</span>
+                                </li>
+                                <li className="flex items-center gap-2">
+                                    <span className="mr-1"><CheckIcon /></span>
+                                    <span className="text-sm">Task 1</span>
+                                </li>
+                                <li className="flex items-center gap-2">
+                                    <span className="mr-1"><CheckIcon /></span>
+                                    <span className="text-sm">Task 1</span>
+                                </li>
+                            </ul>
+                            <div className="flex justify-between p-2 bg-white">
+                                <div className="flex justify-start -space-x-2">
+                                    <img src={profileUrl} alt="profile" className="w-6 h-auto rounded-full" />
+                                    <img src={profileUrl} alt="profile" className="w-6 h-auto rounded-full" />
+                                    <img src={profileUrl} alt="profile" className="w-6 h-auto rounded-full" />   
+                                </div>
+                                <div className="flex justify-end gap-2">
+                                    <div className="flex items-center gap-1"> 
+                                        <span className=""><MessageIcon /></span>
+                                        <span className="text-sm text-[#C4C4C4]">1</span>
+                                    </div>
+                                    <div className="flex items-center gap-1"> 
+                                        <span className=""><PaperClipIcon /></span>
+                                        <span className="text-sm text-[#C4C4C4]">2</span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                        ))}
                     </div>
+                    ))}
                 </div>
             </div>
          </div>
