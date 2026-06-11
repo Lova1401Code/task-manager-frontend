b8fb54c38e90fe70fdbf81c50af9ca9d69cb7601
Lova1401Code <lova.ramiharisoa@gmail.com>
2026-06-11 20:49:09 +0300

    feat: update VideoCameraIcon component to accept color prop and adjust SVG dimensions

--- src/assets/icons/VideoCameraIcon.jsx
+++ src/assets/icons/VideoCameraIcon.jsx
@@ -1,7 +1,8 @@
-export default function VideoCameraIcon() {
+export default function VideoCameraIcon({ color = "#F0F0F0" }) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
-            <path d="M3.25 4A2.25 2.25 0 0 0 1 6.25v7.5A2.25 2.25 0 0 0 3.25 16h7.5A2.25 2.25 0 0 0 13 13.75v-7.5A2.25 2.25 0 0 0 10.75 4h-7.5ZM19 4.75a.75.75 0 0 0-1.28-.53l-3 3a.75.75 0 0 0-.22.53v4.5c0 .199.079.39.22.53l3 3a.75.75 0 0 0 1.28-.53V4.75Z" />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill={color} className="size-4">
+            <path d="M3 4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h5a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H3ZM15 4.75a.75.75 0 0 0-1.28-.53l-2 2a.75.75 0 0 0-.22.53v2.5c0 .199.079.39.22.53l2 2a.75.75 0 0 0 1.28-.53v-6.5Z" />
         </svg>
+
     )
 }
