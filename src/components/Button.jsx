90e571f5a100331c1338b0759b77daf02d6e245c
Lova1401Code <lova.ramiharisoa@gmail.com>
2026-06-23 20:02:56 +0300

    feat: add cursor pointer to Button and MenuItems components for improved interactivity

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 export default function Button({ name, className }) {
     return (
-        <button className={ `text-white rounded-full px-2 py-1 ${className}` }>
+        <button className={ `cursor-pointer  text-white rounded-full px-2 py-1 ${className}` }>
             { name }
         </button>
     )
--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -1,7 +1,7 @@
 export default function MenuItems({ active, menu, clickMenuHandles }) {
     return (
         <li  
-            className={`text-sm ${active ? "text-[#000000] font-semibold border-b-3 border-[#3BA1FF] py-1 px-2" : "text-[#C4C4C4]"}`}
+            className={`cursor-pointer text-sm ${active ? "text-[#000000] font-semibold border-b-3 border-[#3BA1FF] py-1 px-2" : "text-[#C4C4C4]"}`}
             onClick={() => clickMenuHandles(menu)}>
             {menu}
         </li>   
