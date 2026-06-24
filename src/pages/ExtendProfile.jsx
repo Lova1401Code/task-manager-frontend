bb951f252b30687548b2a52b69ea2e8b045f1a1f
Lova1401Code <lova.ramiharisoa@gmail.com>
2026-06-24 22:28:36 +0300

    added cusror pointer in button used by ExtendProfile

--- src/pages/ExtendProfile.jsx
+++ src/pages/ExtendProfile.jsx
@@ -32,18 +32,18 @@ export default function ExtendProfile() {
                 <h2 className="text-xl font-bold">Rober Fox</h2>
                 <h3 className="text-[#3BA1FF] text-sm font-semibold">@robertfox</h3>
                 <p className="text-[#9C9C9C] text-xs text-justify px-6">Massa ornare ultrices nisi in vestibulum. Iaculis commodo amet orci sit non lacus. Odio dui aliquet vitae diam vel tellus feugiat aliquam eu.</p>
-                <button className="px-2 py-1 text-sm rounded-full border border-[#9C9C9C] text-[#9C9C9C]">iOS Developer</button>
+                <button className="cursor-pointer px-2 py-1 text-sm rounded-full border border-[#9C9C9C] text-[#9C9C9C]">iOS Developer</button>
             </div>
             <div className="flex justify-center gap-2 flex-row mt-6">
-                <button className="px-2 py-1 items-center justify-center font-semibold flex gap-1 text-sm">
+                <button className="cursor-pointer px-2 py-1 items-center justify-center font-semibold flex gap-1 text-sm">
                     <AddFriendsIcon color="#3BA1FF" />
                     <span>Add friends</span>
                 </button>
-                <button className="px-2 py-1 items-center justify-center font-semibold flex gap-1 text-sm">
+                <button className="cursor-pointer px-2 py-1 items-center justify-center font-semibold flex gap-1 text-sm">
                     <MessageIcon color="#3BA1FF"/>
                     <span>Message</span>
                 </button>
-                <button className="px-2 py-1 items-center justify-center font-semibold flex gap-1 text-sm">
+                <button className="cursor-pointer px-2 py-1 items-center justify-center font-semibold flex gap-1 text-sm">
                     <PhoneIcon color="#3BA1FF" />
                     <span>Call</span>
                 </button>
