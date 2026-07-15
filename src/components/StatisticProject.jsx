a1e17e4f142c28f1dadc8c1bfa20d0059920ef6b
Lova1401Code <lova.ramiharisoa@gmail.com>
2026-07-15 21:06:24 +0300

    feat: add ArrowUp icon and enhance StatisticProject component layout

--- src/components/StatisticProject.jsx
+++ src/components/StatisticProject.jsx
@@ -1,4 +1,5 @@
-
+import PencilSquare from "../assets/icons/PenciSquare";
+import ArrowUp from "../assets/icons/ArrowUp";
 
 export default function StatisticProject(){
     const dashboardDateProject = {
@@ -19,20 +20,29 @@ export default function StatisticProject(){
     return(
         <div className="flex justify-between py-4">
             { Object.entries(dashboardDateProject.stats).map(([key,stat]) => (
-                <div className="border-1 border-[#9C9C9C] rounded p-2 bg-white" key={key}>
-                    <div className="flex-col">
+                <div className="border-1 border-[#C4C4C4] rounded p-4 bg-white" key={key}>
+                    <div className="flex flex-col gap-4">
                         <div className="text-xs font-bold">
                             {stat.name}
                         </div>
-                        <div className="flex justify-between">
+                        <div className="flex justify-between gap-12">
                             <div className="flex-col">
-                                <div className="flex justify-between">
-                                    <div>{stat.current}</div>
-                                    <div>{stat.previous}</div>
+                                <div className="flex gap-8 items-baseline">
+                                    <div className="font-bold text-lg">{stat.current}</div>
+                                    <div className="flex text-green-500 items-center">
+                                        <ArrowUp />
+                                        <div className="text-green-500 text-xs">
+                                            {stat.current - stat.previous}%
+                                        </div>
+                                    </div>
                                 </div>
-                                <div>vs last week</div>
+                                <div className="text-xs text-gray-500">
+                                    vs last week
+                                </div>
                             </div>
-                            <div>icon here</div>
+                            <div className="flex items-center justify-center rounded-full bg-blue-100 text-blue-500 h-10 w-10">
+                                <PencilSquare />
+                            </div>
                         </div>
                     </div>
                     
