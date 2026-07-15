a32c473daf11d2198dcc0867a5d249d3c5d9b935
Lova1401Code <lova.ramiharisoa@gmail.com>
2026-07-15 07:23:00 +0300

    card of stat not yet finished

--- src/components/StatisticProject.jsx
+++ src/components/StatisticProject.jsx
@@ -1,13 +1,14 @@
 export default function StatisticProject(){
     const dashboardDateProject = {
         stats: {
-            total: {current: 14, previous: 150}, 
-            progress: {current: 50, previous: 60},
-            completed: {current: 64, previous: 90},
-            onHold: {current: 75, previous: 41}
+            total: {name: "total", current: 14, previous: 150}, 
+            progress: {name: "progress", current: 50, previous: 60},
+            completed: {name: "completed", current: 64, previous: 90},
+            onHold: {name: "onHold", current: 75, previous: 41}
         },
         get(metric){
             return {
+                stat: metric,
                 current: this.stats[metric].current,
                 previous: this.stats[metric].previous
             }
@@ -16,7 +17,24 @@ export default function StatisticProject(){
     return(
         <div className="flex justify-between py-4">
             { Object.entries(dashboardDateProject.stats).map(([key,stat]) => (
-                <div className="border-1 border-[#9C9C9C] rounded p-4 bg-white" key={key}>statistic here {stat.current}</div>
+                <div className="border-1 border-[#9C9C9C] rounded p-2 bg-white" key={key}>
+                    <div className="flex-col">
+                        <div className="text-xs font-bold">
+                            {stat.name}
+                        </div>
+                        <div className="flex justify-between">
+                            <div className="flex-col">
+                                <div className="flex justify-between">
+                                    <div>{stat.current}</div>
+                                    <div>{stat.previous}</div>
+                                </div>
+                                <div>vs last week</div>
+                            </div>
+                            <div>icon here</div>
+                        </div>
+                    </div>
+                    
+                </div>
             )) }
         </div>
     )
